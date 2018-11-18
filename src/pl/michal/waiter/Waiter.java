4c6aa5a97af7366d0761339a294eadb84f0920c7
Michal Nowakowski <michal951now@gmail.com>
2018-11-18 20:39:29 +0100

    changed whole logic in waiter

--- src/pl/michal/waiter/Waiter.java
+++ src/pl/michal/waiter/Waiter.java
@@ -17,56 +17,47 @@ public class Waiter {
 
 		this.forkQuantity = forkQuantity;
 		this.forks = new ArrayList<Fork>();
-		
+
 		for (int i = 0; i < forkQuantity; i++) {
 			forks.add(new Fork());
 		}
 
 		this.knifeQuantity = knifeQuantity;
 		this.knives = new ArrayList<Knife>();
-		
+
 		for (int i = 0; i < knifeQuantity; i++) {
 			knives.add(new Knife());
 		}
 	}
 
-	public synchronized Fork provideFork() {
-		for (Fork fork : forks) {
-			if (!fork.isBeingUsed()) {
-				return fork;
-			}
-		}
-
-		return null;
-	}
-
-	public synchronized Knife provideKnife() {
-		for (Knife knife : knives) {
-			if (!knife.isBeingUsed()) {
-				knife.pickUp();
-				return knife;
-			}
-		}
-
-		return null;
-	}
-
-	public synchronized void takeBackFork() {
-		for (Fork fork : forks) {
-			if (fork.isBeingUsed()) {
-				fork.putDown();
-				break;
+	public synchronized void provideFork() {
+		if (forks.size() != 0)
+			forks.remove(0);
+		else {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 	}
 
-	public synchronized void takeBackKnife() {
-		for (Knife knife : knives) {
-			if (knife.isBeingUsed()) {
-				knife.putDown();
-				break;
+	public synchronized void provideKnife() {
+		if (knives.size() != 0)
+			knives.remove(0);
+		else {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
+	}
+
+	public synchronized void takeBackCutlery() {
+		forks.add(new Fork());
+		knives.add(new Knife());
+		notify();
 	}
 
 }
