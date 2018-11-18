1762a500a92f160edf40743f70e27b2a8ec01274
Michal Nowakowski <michal951now@gmail.com>
2018-11-18 20:39:00 +0100

    refactored run method

--- src/pl/michal/philosoper/Philosopher.java
+++ src/pl/michal/philosoper/Philosopher.java
@@ -11,6 +11,7 @@ public class Philosopher implements Runnable {
 	private Knife knife;
 	private int hungryLevel;
 	private String name;
+	private int eatTurns = 0;
 
 	public Philosopher(Waiter waiter, String name) {
 		this.waiter = waiter;
@@ -22,38 +23,46 @@ public class Philosopher implements Runnable {
 	public void run() {
 
 		System.out.println(name + " IS THINKING...");
-		int eatTurns = 0;
-		while (eatTurns  < 3) {
-			fork = waiter.provideFork();
-			knife = waiter.provideKnife();
-			if (!(fork == null) && !(knife == null)) {
-				while (hungryLevel < 5) {
+		//while (eatTurns < 3) {
+			waiter.provideFork();
+			waiter.provideKnife();
+			try {
+				Thread.sleep((long) (4000 * Math.random() + 1000));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 
-					System.out.println(name + " took knife and fork");
+			System.out.println(name + " took knife and fork");
+			while (hungryLevel < 3) {
+				try {
+					Thread.sleep((long) (3000 * Math.random() + 1000));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 
-					try {
-						Thread.sleep((long) (3000 * Math.random() + 1000));
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-
-					System.out.println(name + " IS EATING");
+				System.out.println(name + " IS EATING");
+				hungryLevel++;
 
 					try {
 						Thread.sleep((long) (4000 * Math.random() + 1000));
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-					hungryLevel++;
-				}
-				System.out.println(name + " IS BACK TO THINKING");
-			}
 
+			}
 			hungryLevel = 0;
-			
-			eatTurns++;
-		}
-		
+			//eatTurns++;
+			System.out.println(name + " GIVING BACK CUTLERY");
+			waiter.takeBackCutlery();
+			System.out.println(name + " IS BACK TO THINKING");
+
+			try {
+				Thread.sleep((long) (3000 * Math.random() + 1000));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		//}
+
 		System.out.println("\n\n" + name + " is full. He can work in peace \n\n");
 	}
 
