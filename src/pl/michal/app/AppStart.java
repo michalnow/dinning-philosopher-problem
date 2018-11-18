1595f941a0e318300e38b60f1226de8f73eeaae4
Michal Nowakowski <michal951now@gmail.com>
2018-11-18 14:12:56 +0100

    added array of string, created threads etc

--- src/pl/michal/app/AppStart.java
+++ src/pl/michal/app/AppStart.java
@@ -1,14 +1,26 @@
 package pl.michal.app;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import pl.michal.fork.Fork;
-import pl.michal.knife.Knife;
 import pl.michal.philosoper.Philosopher;
 import pl.michal.waiter.Waiter;
 
 public class AppStart {
+	final static String[] names = { "Archimedes", "Sokrates", "Platon", "Arystoteles", "Zenon", "Anaksagoras" };
+	final static int PHILOSOPHERS_QUANTITY = 5;
+	final static int KNIVES_QUANTITY = 3;
+	final static int FORKS_QUANTITY = 4;
 
+	public static void main(String[] args) {
+		Waiter waiter = new Waiter(FORKS_QUANTITY, KNIVES_QUANTITY);
+
+		Philosopher[] philosophers = new Philosopher[PHILOSOPHERS_QUANTITY];
+
+		for (int i = 0; i < PHILOSOPHERS_QUANTITY; i++) {
+			philosophers[i] = new Philosopher(waiter, names[i]);
+
+			Thread thread = new Thread(philosophers[i], names[i]);
+			thread.start();
+		}
+	}
 
 }
