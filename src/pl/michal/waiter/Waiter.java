5a6b846f1af3c1f5a6bbbd86d9bfda23595e9998
Michal Nowakowski <michal951now@gmail.com>
2018-11-18 14:14:39 +0100

    changed one line where was small problem in if

--- src/pl/michal/waiter/Waiter.java
+++ src/pl/michal/waiter/Waiter.java
@@ -7,61 +7,66 @@ import pl.michal.fork.Fork;
 import pl.michal.knife.Knife;
 
 public class Waiter {
-	
+
 	private int forkQuantity;
 	private int knifeQuantity;
 	private List<Knife> knives;
 	private List<Fork> forks;
-	
+
 	public Waiter(int forkQuantity, int knifeQuantity) {
-		for(int i=0;i<forkQuantity;i++) {
+
+		this.forkQuantity = forkQuantity;
+		this.forks = new ArrayList<Fork>();
+		
+		for (int i = 0; i < forkQuantity; i++) {
 			forks.add(new Fork());
 		}
+
+		this.knifeQuantity = knifeQuantity;
+		this.knives = new ArrayList<Knife>();
 		
-		for(int i=0;i<knifeQuantity;i++) {
+		for (int i = 0; i < knifeQuantity; i++) {
 			knives.add(new Knife());
 		}
 	}
-	
+
 	public synchronized Fork provideFork() {
-		for(Fork fork: forks) {
-			if(!fork.isBeingUsed()) {
+		for (Fork fork : forks) {
+			if (!fork.isBeingUsed()) {
 				return fork;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	public synchronized Knife provideKnife() {
-		for(Knife knife: knives) {
-			if(!knife.isBeingUsed()) {
+		for (Knife knife : knives) {
+			if (!knife.isBeingUsed()) {
 				knife.pickUp();
 				return knife;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	public synchronized void takeBackFork() {
-		for(Fork fork: forks) {
-			if(fork.isBeingUsed()) {
+		for (Fork fork : forks) {
+			if (fork.isBeingUsed()) {
 				fork.putDown();
 				break;
 			}
 		}
 	}
-	
+
 	public synchronized void takeBackKnife() {
-		for(Knife knife: knives) {
-			if(!knife.isBeingUsed()) {
+		for (Knife knife : knives) {
+			if (knife.isBeingUsed()) {
 				knife.putDown();
 				break;
 			}
 		}
-	}	
- 
-	
-	
+	}
+
 }
